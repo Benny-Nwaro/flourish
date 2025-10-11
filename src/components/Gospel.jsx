8ab80277bd340cb40ade43a51c277d8546843b02
Iheyinwa <caruogu@outlook.com>
2025-10-11 04:27:32 +0100

    feat: fix expressions page

--- src/components/Gospel.jsx
+++ src/components/Gospel.jsx
@@ -1,12 +1,17 @@
 import PropTypes from "prop-types";
+import greenstar from "../assets/icons/halfgreenstar.png";
+import yellowstar from "../assets/icons/yellowStar.png";
+import line from "../assets/icons/expressionsLine.png";
 function CountryCard({ count, label }) {
   return (
     <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
-      <div className="flex flex-col grow justify-center items-center px-9 py-10 w-full text-center text-black whitespace-nowrap rounded-xl bg-stone-50 max-md:px-5 max-md:mt-10">
-        <div className="text-7xl tracking-tighter leading-[70.547px] max-md:text-4xl">
+      <div className="flex flex-col grow justify-center items-center px-9 py-10 w-full text-center text-black whitespace-nowrap rounded-xl bg-[#F8F8F8] max-md:px-5 max-md:mt-10">
+        <p className="md:text-[78.85px] font-stemBold tracking-[-2.5%] leading-[89%] text-6xl">
           {count}
-        </div>
-        <div className="mt-1 text-2xl tracking-tight leading-9">{label}</div>
+        </p>
+        <p className="ext-[25.26px] font-stemRegular tracking-[-1%] leading-[143%]">
+          {label}
+        </p>
       </div>
     </div>
   );
@@ -25,39 +30,41 @@ export default function Gospel() {
   ];
 
   return (
-    <section className="flex justify-center items-center px-32  max-md:px-5 bg-slate-200">
-      <div className="flex flex-col mt-20 pb-12 rounded-3xl justify-center items-center text-center bg-neutral-50">
-        <div className="flex gap-5 justify-between w-full max-md:flex-wrap max-md:max-w-full">
-          <div className="flex flex-col max-md:max-w-full">
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/bf619dcea7c79a0defaa97e7518ef603a63beeb23f496a1ddb9f2bd8a7c4e58d?apiKey=873e47fe7796454e93671642132d9742&"
-              alt=""
-              className="border-white border-solid aspect-[0.58] border-[5px] fill-emerald-500 stroke-[4.568px] stroke-white w-[42px]"
-            />
-            <div className="flex flex-col self-end px-5 mt-2 max-w-full  max-md:mt-10">
-              <h1 className="text-7xl font-kuano tracking-tighter text-blue-600 leading-[70px] max-md:max-w-full max-md:text-4xl max-md:leading-10">
-                Until the Gospel fills the earth{" "}
-                <span className="text-blue-600">
-                  as the waters cover the sea
-                </span>
-              </h1>
-              <p className="mt-7 text-base tracking-normal leading-6 text-black max-md:max-w-full">
-                Who we are Who we are Who we are Who we are Who we are Who we
-                are Who we are Who we are Who we are Who we areWho we are Who we
-                are Who we are Who we are Who we are Who we areWho we are Who we
-                are Who we are Who we are
-              </p>
-            </div>
-          </div>
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/fe11ca80106f144999b34b9f73d8513d89e7d6801d4ee9cc708274bb775dd8d0?apiKey=873e47fe7796454e93671642132d9742&"
-            alt=""
-            className="shrink-0 self-start mt-28 border-white border-solid aspect-[0.61] border-[5px] fill-yellow-400 stroke-[5px] stroke-white w-[51px] max-md:mt-10"
-          />
+    <section className="flex justify-center items-center px-[20px] md:px-[80px] xl:px-[150px]">
+      <div className="flex flex-col justify-center gap-[30px] mt-20 pt-[150px] pb-[50px] rounded-[25px] relative bg-[#FAFAFA] h-full">
+        <img
+          loading="lazy"
+          src={greenstar}
+          alt=""
+          className="absolute top-5 left-0 w-fit h-fit"
+        />
+        <div className="flex flex-col relative left-[20px] md:left-[80px] xl:left-[151px] top-1/3 gap-4 w-[90%] min-[500px]:w-[70%] xl:w-[55%]">
+          <h1 className="md:text-[78.28px] font-dmserif tracking-[-2.5%] leading-[89%] text-6xl">
+            Until the Gospel fills the earth{" "}
+          </h1>
+          <h1 className="text-brandBlue md:text-[78.28px] font-dmserif tracking-[-2.5%] leading-[89%] text-6xl">
+            as the waters cover the sea
+          </h1>
+          <p className=" tracking-[-1%] leading-[143%] text-black max-md:max-w-full font-dmserif">
+            Who we are Who we are Who we are Who we are Who we are Who we are
+            Who we are Who we are Who we are Who we areWho we are Who we are Who
+            we are Who we are Who we are Who we areWho we are Who we are Who we
+            are Who we are
+          </p>
         </div>
-        <div className="justify-between self-center p-9 mt-16 w-full bg-white rounded-3xl max-w-[1063px] max-md:px-5 max-md:mt-10 max-md:max-w-full">
+        <img
+          loading="lazy"
+          src={yellowstar}
+          alt=""
+          className="w-fit h-fit absolute right-0 top-[156px]"
+        />
+        <img
+          loading="lazy"
+          src={line}
+          alt=""
+          className="w-fit h-fit absolute -right-24 lg:-right-36 hidden min-[500px]:block top-[320px] z-50"
+        />
+        <div className="justify-between relative p-9 bottom-0  bg-white rounded-3xl w-[95%] max-md:px-5 mx-auto">
           <div className="flex gap-5 max-md:flex-col max-md:gap-0">
             {countryData.map((data, index) => (
               <CountryCard key={index} count={data.count} label={data.label} />
